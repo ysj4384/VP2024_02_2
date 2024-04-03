8ea316c01fee512bf222f222720ae02cd6acb30f
ysj4384 <165129871+ysj4384@users.noreply.github.com>
2024-04-03 14:42:27 +0900

    Add files via upload

---
+++ 016_repaeat_2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _016_repaeat_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //(43장) x에서y까지의 합, 홀수 합, 역수 합\
+            //(45장) 구구단
+            //(47장) x의 y 승
+            //(53장) 팩토리얼
+            // double sum = 0;
+            /*for(int i = i, i <= 100, i++)
+             *{
+             *sum += I;
+             *}*/
+
+            /* Console.Write("x : ");
+             int x = int.Parse(Console.ReadLine());
+             Console.Write("y : ");
+             int y = int.Parse(Console.ReadLine());
+
+
+             for(int i = x; i <= y; i++)
+             {
+                 sum += i;
+             }
+             Console.WriteLine(sum);*/
+
+            /*Console.WriteLine("홀수의합");
+             * for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);*/
+
+            /*Console.WirteLine("역수의 합");
+             * for(int i = 1; i < 10; i++)
+             {
+                 sum += 1.0 / i;
+             }
+             Console.WriteLine(sum);*/
+
+            //Console.Write("구구단수 입력 : ");
+            //int gugudan = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <=9; i++) 
+            {
+                for(int j =2; j <= 9; j++)
+                {
+                        Console.Write("{0} X {1} = {2,2}\t", j, i, i * j);
+                }
+                Console.WriteLine();
+            }
+            /*Console.Write("X : ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Y : ");
+            int y = int.Parse(Console.ReadLine());
+            int r = 1;
+
+            for (int i = 1; i <= y; i++)
+            {
+                r *= x;
+                Console.WriteLine("{0}의 {1}승 : {2}",x, i, r);
+            }
+            Console.WriteLine("구하고싶은 팩토리얼 값 : ");
+            int i = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int j = 1; j<=i;j++)
+            {
+                int fact = 1;
+                for(int n = 2; n<=j;n++)
+                {
+                    fact *= n;
+                }
+                sum += fact;
+                Console.WriteLine("{0, 2}! = {1, 10:#,#}",j, fact);
+            }
+            Console.WriteLine("1! + 2! + ... + {0}! = {1:N0}\n", i, sum);*/
+        }
+    }
+}
---
+++ 017_arrange/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _017_arrange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+
+            for(int i = 0; i<a.Length; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+                //Console.WriteLine("\n");
+            }
+
+            /* 원소를 모두 출력
+            Console.WriteLine("\n a의 원소");
+            // 1) 인덱스 사용
+            for (int i = 0; i<10; i++)
+                Console.Write(a[i] + " ");
+            Console.WriteLine();
+
+            // 2) foreach 사용
+            foreach (var value in a)
+            {
+                Console.Write(value + " ");
+                Console.WriteLine();
+            }*/
+
+            /* 원소의 합 출력
+            int sum = 0;
+            for(int i = 0;i<10; i++)
+            {
+                sum += a[i];
+            }
+            Console.Write("{0}", sum);
+            */
+
+            // 가장 큰 값을 출력
+            int max = a[0];
+
+            for(int i =0;i<10; i++)
+            {
+                if(max < a[i])
+                {
+                    max = a[i];
+                }
+            }
+            Console.WriteLine("최댓값 {0}", max);
+        }
+    }
+}
